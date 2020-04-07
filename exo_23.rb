cf0311526341e819a8a991da4408f338b6a72730
Julien Lavoisier <jul.lavoisier@gmail.com>
2020-04-07 16:46:58 +0200

    very last with last exo

---
+++ exo_23.rb
@@ -0,0 +1,11 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (entre 1 et 25)"
+puts "> "
+n = gets.chomp.to_i
+puts "Voici la pyramide"
+i=1
+j=n
+while i <= n
+    puts " " * j + "*" * i
+    i = i + 1
+    j = j - 1
+end
